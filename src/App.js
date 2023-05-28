9fe10771dce098dfa605875f5e22a8a27dbcae7e
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-05-27 20:28:29 -0500

    add new features

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import axios from "axios"
 
 function App() {
 
-  const[quote, setQuote] = useState("")
+  const [quote, setQuote] = useState("")
   const [author, setAuthor] = useState("")
+  const [button, setButton] = useState(true)
+  const [buttonClicked, setButtonClicked] = useState(false)
 
   const getQuote = () => {
     axios
@@ -14,15 +16,36 @@ function App() {
         console.log(response.data)
         setQuote(response.data.content)
         setAuthor(response.data.author)
+        localStorage.setItem("lastClickedDate", getTodayDate())
       })
       .catch((error) => {
         console.log(error)
       })
     }
 
+  const checkClicked = () => {
+    setButtonClicked(true)
+  }
+
+  //useEffect will be executed whenever buttonClicked state changes
   useEffect(() => {
-    getQuote()
-  }, [])
+    if (buttonClicked) {
+      getQuote()
+      setButtonClicked(false)
+    }
+  }, [buttonClicked])
+
+  useEffect(() => {
+    const lastClickedDate = localStorage.getItem("lastClickedDate")
+    if (lastClickedDate == getTodayDate()) {
+      setButton(false)
+    }
+  })
+
+  const getTodayDate = () => {
+    const date = new Date()
+    return date.toISOString().split("T")[0]
+  }
 
   return (
     <div className="App">
@@ -30,10 +53,24 @@ function App() {
           <header>Get a Quote Today!</header>
       </div>
       <div className = "quoteSection">
+        {!button ? (
+          <>
           <div className = "actualQuote">{quote}</div> 
           <div className = "author">--{author}--</div>
+          </>
+        ) : (
+          <>
+          <div className = "placeHolder"></div>
+          </>
+        )}
       </div>
-          <button className = "button" onClick = {getQuote}>Click to get a new quote</button>
+      {button && (
+         <button className = "button" onClick = {checkClicked}>Click to get a new quote</button>
+      )}
+      <div className = "bottom">
+        <a class = "link"href = "https://github.com/pth1209/daily_quote_generator.git">Source Code!</a>
+      </div>
+
     </div>
   );
 }
