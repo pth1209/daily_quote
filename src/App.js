1150196ad8629774a086e1066054a688d8b838b2
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-05-27 23:36:38 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 function App() {
-
   const [quote, setQuote] = useState("")
   const [author, setAuthor] = useState("")
   const [button, setButton] = useState(true)
@@ -17,6 +16,7 @@ function App() {
         setQuote(response.data.content)
         setAuthor(response.data.author)
         localStorage.setItem("lastClickedDate", getTodayDate())
+        localStorage.setItem("ButtonClicked", buttonClicked)
       })
       .catch((error) => {
         console.log(error)
@@ -35,6 +35,7 @@ function App() {
     }
   }, [buttonClicked])
 
+  //check if current date is equal to the date of when the button was clicked
   useEffect(() => {
     const lastClickedDate = localStorage.getItem("lastClickedDate")
     if (lastClickedDate == getTodayDate()) {
@@ -42,36 +43,40 @@ function App() {
     }
   })
 
+  //extract the current date
   const getTodayDate = () => {
     const date = new Date()
     return date.toISOString().split("T")[0]
   }
 
   return (
-    <div className="App">
+    <div className = "App">
       <div className = "header">
           <header>Get a Quote Today!</header>
       </div>
+
       <div className = "quoteSection">
-        {!button ? (
+        {!buttonClicked ? (
           <>
           <div className = "actualQuote">{quote}</div> 
-          <div className = "author">--{author}--</div>
+          <div className = "author">{author}</div>
           </>
         ) : (
           <>
-          <div className = "placeHolder"></div>
+          <div className = "placeholder"></div>
           </>
         )}
       </div>
+      <div className = "buttonDiv">
       {button && (
          <button className = "button" onClick = {checkClicked}>Click to get a new quote</button>
       )}
+      </div>
       <div className = "bottom">
         <a class = "link"href = "https://github.com/pth1209/daily_quote_generator.git">Source Code!</a>
-      </div>
 
-    </div>
+        </div>
+      </div>
   );
 }
 
